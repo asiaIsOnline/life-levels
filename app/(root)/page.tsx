d89fc2c3f4669a486a86ab2a51dfb16c77e9df32
asiasAtWork <asiasatwork@gmail.com>
2023-09-21 23:32:08 -0500

    Update default Next.js environment

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Modern Market Manager</h1>
+            <Button>Sites</Button>
+        </div>
+    )
+  }
+  
